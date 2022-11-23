2294219a768b953d889be3598ebc67c45f6278e5
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-11-23 18:07:55 +0900

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["images.unsplash.com", "plus.unsplash.com"],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
