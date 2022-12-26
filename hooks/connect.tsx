09c883bebcbad3ec741372a7a777e99da7d80d8f
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-12-26 19:33:37 +0900

    deploy on shibuya

--- hooks/connect.tsx
+++ hooks/connect.tsx
@@ -19,8 +19,8 @@ type Props = {
 // コントラクトとの接続を行うための関数
 export const connectToContract = async (props: Props) => {
   // rpcのURL
-  const blockchainUrl = "ws://127.0.0.1:9944";
-  // const blockchainUrl = "wss://shibuya-rpc.dwellir.com";
+  // const blockchainUrl = "ws://127.0.0.1:9944";
+  const blockchainUrl = "wss://shibuya-rpc.dwellir.com";
 
   // この関数でアカウント情報を取得する
   const extensionSetup = async () => {
